fd87fdbadc2b6a2cf54d7f3718c78b4eaa281e6c
choizeus02 <choizeus0212@gmail.com>
2024-07-05 15:32:48 +0900

    [CORS문제 해결 테스트2] WebConfig 설정

--- src/main/java/choizeus/study/config/WebConfig.java
+++ src/main/java/choizeus/study/config/WebConfig.java
@@ -6,12 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final String CORS_URL_PATTERN = "/**";
+    private static final String CORS_URL = "http://116.121.184.161:3000,http://localhost:3000";
+    private static final String CORS_METHOD = "*";
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping(CORS_URL_PATTERN)
+                .allowedOrigins(CORS_URL)
+                .allowedMethods(CORS_METHOD);
     }
 }
