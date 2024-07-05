6ef78509a559853fc979570dd6aa8616d15e5f86
choizeus02 <choizeus0212@gmail.com>
2024-07-05 16:07:22 +0900

    [CORS문제 해결 테스트5] WebConfig 설정

--- src/main/java/choizeus/study/config/WebConfig.java
+++ src/main/java/choizeus/study/config/WebConfig.java
@@ -1,10 +1,12 @@
 package choizeus.study.config;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private static final String CORS_URL_PATTERN = "/**";
@@ -14,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(CORS_URL_PATTERN)
+                .allowedHeaders("*")
                 .allowedOrigins(CORS_URL)
-                .allowedMethods(CORS_METHOD)
-                .allowCredentials(true);
+                .allowedMethods(CORS_METHOD);
     }
 }
